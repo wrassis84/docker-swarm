f26d0495c30d3aff835634890f13ea001d6e566e
William Rezende <wrassis84@gmail.com>
2023-05-15 21:01:48 -0300

    feat: add bash script to automate all

---
+++ Deploy.sh
@@ -0,0 +1,45 @@
+######## ENVIRONMENT ###########################################################
+
+#!/usr/bin/env bash
+
+######## ABOUT #################################################################
+
+# Repository       : https://github.com/wrassis84/<REPO>
+# Author           : William Ramos de Assis Rezende
+# Maintainer       : William Ramos de Assis Rezende
+#
+# DeployServers.sh : Executes Ansible Playbooks to Config Servers.
+# Requirements     : bash, ansible
+# Usage            : ./Deploy.sh
+
+######## TESTING ENVIRONMENT ###################################################
+
+######## TESTS/VALIDATIONS #####################################################
+
+######## VARIABLES #############################################################
+
+######## FUNCTIONS #############################################################
+CleanSSH () {
+  for i in $(seq 0 2); do
+    ssh-keygen -f "$HOME/.ssh/known_hosts"         \
+               -R "192.168.0.23$i" > /dev/null 2>&1;
+  done           
+}
+######## MAIN CODE - START #####################################################
+echo "Configuring ansible environment..."
+cd $PWD/ansible
+source ansible.env > /dev/null
+
+echo "Configuring ssh hosts..."
+CleanSSH
+
+echo "Playing setup_ubuntu.yml..."
+ansible-playbook setup_ubuntu.yml -u sysadmin -b #> /dev/null
+
+echo "Playing setup_docker.yml"
+ansible-playbook setup_docker.yml -u sysadmin -b #> /dev/null
+cd ../
+######## MAIN CODE - END #######################################################
+# TODO:
+# Test if ansible is installed;
+# Checks that servers are running and reachable;
